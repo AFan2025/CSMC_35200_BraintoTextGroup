8fb8a823885c0e35d3de79ee4282fa8ae9cc5c6a
Alex Fan <alexfan@MacBook-Pro-38.local>
2026-05-28 09:39:05 -0500

    added scaling to embedding so noise doesn't overtake immediate

--- DiffNeuralDecoder/diffusionNeuralDecoder/diffusion_model.py
+++ DiffNeuralDecoder/diffusionNeuralDecoder/diffusion_model.py
@@ -382,7 +382,7 @@ class PhonemeDiT(nn.Module):
                 nn.init.zeros_(self.final_layer.linear.bias)
 
     def embed_tok(self, x):
-        return self.x_embedder(x)
+        return self.x_embedder(x) * math.sqrt(self.d_model)
     
     @torch.no_grad()
     def decode_tok(self, x):
